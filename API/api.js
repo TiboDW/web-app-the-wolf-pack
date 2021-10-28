c72e2c93dcdc656f6248abc8bdfa03075d9cb04a
Abdirahman <rizetracker@gmail.com>
2021-10-28 19:40:57 +0200

    GET movies route + GET movies by id route

--- API/api.js
+++
@@ -1,5 +0,0 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
-
-const URL = "https://tickets.cinemafocus.be/nl/movies";
-
--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const {getAllMovies, getMovieDetails} = require("./API/movies.js");
 
+// GET all movies
+app.get("/movies", async(req,res) => {
+   try {
+        const results = await getAllMovies();
+        res.status(200).send(results);
+    }
+   catch(err){
+        res.status(500).send("Internal Server error");
+   }
+})
 
+// GET movie by id
+app.get("/movies/:id", async(req,res) => {
+    try {
+        const details = await getMovieDetails(req.params.id);
+        res.status(200).send(details);
+    }
+    catch(err){
+        res.status(404).send("NOT FOUND");
+    }
+ })
 
-
-// listen step
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
 });
