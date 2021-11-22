b84b14654c528f0aa9ce382d31fdfe4ef44a6970
LorenzoWilliquetEHB <lorenzo.williquet@student.ehb.be>
2021-11-22 13:37:08 +0100

    Weekoverzicht updated en footers fixed

--- frontend/src/pages/WordtVerwacht.js
+++ frontend/src/pages/WordtVerwacht.js
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 
 export const WordtVerwacht = () => {
     return (
-        <div className="App font-bold">
-        <div class="container mx-auto px-6 mt-16 text-left text-color-footer">
-          Wordt verwacht
-          <div class=" border-t-2 border-gray-300 flex flex-col items-left">
-            <div class="grid grid-cols-4 gap-2 place-content-start mt-10 overflow-auto no-scroll h-96 w-auto">
+        <div className="App font-bold flex-grow mb-3 text-color-footer">
+        
+            <div class="grid grid-cols-4 gap-2 place-content-start mt-10 overflow-auto w-auto">
               <div class="ml-20 mb-5">
               <Link to="/film"><img
                 class="h-72 w-auto float-left"
@@ -79,8 +77,6 @@ export const WordtVerwacht = () => {
                 <p class="w-44 truncate">Venom: Let there be carnage</p>
               </div>
             </div>
-          </div>
-        </div>
       </div>
     )
 }
