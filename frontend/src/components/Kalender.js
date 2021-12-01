7aef409e65d97b3ed16413423558e68c24f4b9e2
LorenzoWilliquetEHB <lorenzo.williquet@student.ehb.be>
2021-12-01 22:34:11 +0100

    Update Kalender

--- frontend/src/components/Kalender.js
+++ frontend/src/components/Kalender.js
@@ -12,11 +12,10 @@ import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { isNullOrUndefined, L10n } from "@syncfusion/ej2-base";
 import { getMovieById } from "../api/movies";
-import {useLocation} from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { UpdateMovie } from "../api/movies";
 import { initializeApp } from "firebase/app";
-import { getStorage, ref , uploadBytes,getDownloadURL  } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -25,53 +24,38 @@ const config = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
-};  
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
 
 const firebaseApp = initializeApp(config);
 
 L10n.load({
   "en-US": {
     schedule: {
-      newEvent: "Film toevoegen",
+      newEvent: "Film event toevoegen",
+      editEvent: "Film event aanpassen",
+      deleteEvent: "Film event verwijderen",
+      delete:"verwijderen",
+      cancel:"annuleren"
     },
   },
 });
 
 export const Kalender = () => {
+
   const [movies, setMovies] = useState([]);
 
-  const location = useLocation();
-  const id = location.pathname.split("/")[3];
+  var id;
+  
+  const { getAccessTokenSilently } = useAuth0();
 
-  const [movie, setMovie] = useState(undefined);
-  const [titel, setTitel] = useState(undefined);
-  const [img , setImg] = useState(undefined);
-  const [description, setDescription] = useState(undefined);
-  const [regisseur, setRegisseur] = useState(undefined);
-  const [cast , setCast] = useState(undefined);
-  const [trailer, setTrailer] = useState(undefined);
-  const [duur , setDuur] = useState(undefined);
-  const [temp, setTemp] = useState(undefined);
-  const {getAccessTokenSilently} = useAuth0();
-
-  useEffect(() => {
-    getMovieById(id).then(movie => {
-      setMovie(movie);
-      setTitel(movie.titel);
-      setImg(movie.img_url);
-      setDescription(movie.description);
-      setRegisseur(movie.regisseur);
-      setCast(movie.hoofdrollen.join(", "));
-      setTrailer(movie.trailer_url);
-      setDuur(movie.duur);
-    });
-  }, [id]);
+ 
 
   useEffect(() => {
     getAllMovies().then((movies) => setMovies(movies));
   }, []);
 
+  // Het begin uur van een film terug geven
   function getStartTimeMovie(i, j) {
     var datum = new Date(
       movies
@@ -88,7 +72,7 @@ export const Kalender = () => {
     datum.setHours(parseInt(uur));
     return datum;
   }
-
+  // Het eind uur gaan terug geven van een film
   function getEndTimeMovie(i, j) {
     var datum = new Date(
       movies
@@ -105,29 +89,20 @@ export const Kalender = () => {
     datum.setHours(parseInt(uur) + 3);
     return datum;
   }
-
-  function getZaal(){
-    return  movies
-    .filter((movie) => movie.isReleased)
-    .map((movie) => movie.vertoningen.map((vertoning) => vertoning.zaal)[j])[i];
+  // De zaal gaan terug geven van een film van een bepaalde vertoning
+  function getZaal() {
+    return movies
+      .filter((movie) => movie.isReleased)
+      .map((movie) => movie.vertoningen.map((vertoning) => vertoning.zaal)[j])[
+      i
+    ];
   }
 
+  // Array van films die in kalender verwerkt wordt
   var dataKalender = [];
-
-  for (
-    var i = 0;
-    i <= movies.filter((movie) => movie.isReleased).length - 1;
-    i++
-  ) {
-    for (
-      var j = 0;
-      j <=
-      movies
-        .filter((movie) => movie.isReleased)
-        .map((movie) => movie.vertoningen).length -
-        1;
-      j++
-    ) {
+  // Editor template opvullen met de bestaande data
+  for (var i = 0; i <= movies.filter((movie) => movie.isReleased).length - 1;i++) {
+    for (var j = 0; j <= movies.filter((movie) => movie.isReleased).map((movie) => movie.vertoningen).length - 1; j++) {
       var data = {
         Id: movies
           .filter((movie) => movie.isReleased)
@@ -137,12 +112,64 @@ export const Kalender = () => {
           .map((movie) => movie.titel)[i],
         StartTime: getStartTimeMovie(i, j),
         EndTime: getEndTimeMovie(i, j),
-        Zaal: getZaal(i,j),
+        Zaal: getZaal(i, j),
       };
       dataKalender.push(data);
     }
   }
 
+   // Nodig voor de data in de vertoningen te steken
+   function getLocalDateString(date) {
+    return date.toLocaleDateString("nl-BE", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      weekday: "long",
+    });
+  }
+
+  // Als de editortemplate gesloten wordt de data gaan weergeven
+  // Als de editortemplate sluit de vertoning update met de extra data en de kalender refreshen (Is beter)
+  const popupClose = (args) => {
+    var film;
+    if (args.type === "Editor" && !isNullOrUndefined(args.data)) {
+      // De film die gespeeld wordt
+      let filmElement = args.element.querySelector("#Film");
+      if (filmElement) {
+        args.data.Film = filmElement.value;
+        film = movies.filter(movie => movie.isReleased).find(movie => movie.titel === filmElement.value);
+
+        // Begin uur en eind uur + de datum van de film die gespeeld wordt
+        var end = new Date(args.data.StartTime);
+        end.setHours(end.getHours() + 3);
+        args.data.EndTime = end;
+         // De zaal waar de film gespeeld wordt
+        let zaalElement = args.element.querySelector("#Zaal");
+        if (zaalElement) {
+          args.data.Zaal = zaalElement.value;
+
+          //Code voor het update van de film
+          const movie = await getMovieById(film._id)
+
+          const accessToken = await getAccessTokenSilently();
+          await UpdateMovie(film._id,accessToken,);
+          window.location.reload();
+        }
+      }
+      //Geeft de datum terug voor in vertoningen te steken
+      //console.log(getLocalDateString(new Date(args.data.StartTime)).split(" ").slice(1).join(" "));
+      //Geeft de dag terug voor in vertoningen te steken
+      //console.log(getLocalDateString(new Date(args.data.StartTime)).split(" ")[0]);
+      //Geeft het uur terug voor in vertoningen te steken
+      //console.log(new Date(args.data.StartTime).toTimeString().substring(0,5))
+
+      console.log(film._id);
+      console.log(movies.filter((movie) => movie.isReleased).find((movie) => movie.titel === args.data.Film));
+      console.log(args.data);
+    }
+  };
+
+  // HTML Editortemplate weergave die de admin kan invullen
   const editorTemplate = (props) => {
     return props !== undefined ? (
       <table
@@ -155,6 +182,7 @@ export const Kalender = () => {
             <td style={{ colspan: "4" }}>
               <DropDownListComponent
                 id="Film"
+                placeholder="Kies film"
                 data-name="Subject"
                 className="e-field"
                 style={{ width: "100%" }}
@@ -165,7 +193,7 @@ export const Kalender = () => {
             </td>
           </tr>
           <tr>
-            <td className="e-textlabel">From</td>
+            <td className="e-textlabel">Start uur</td>
             <td colSpan={4}>
               <DateTimePickerComponent
                 format="dd/MM/yy hh:mm a"
@@ -176,13 +204,13 @@ export const Kalender = () => {
               ></DateTimePickerComponent>
             </td>
           </tr>
-        
+
           <tr>
             <td className="e-textlabel">Zaal</td>
             <td style={{ colspan: "4" }}>
               <DropDownListComponent
                 id="Zaal"
-                placeholder= 'Kies zaal'
+                placeholder="Kies zaal"
                 data-name="Zaal"
                 className="e-field"
                 style={{ width: "100%" }}
@@ -195,41 +223,6 @@ export const Kalender = () => {
     ) : (
       <div />
     );
-  };
-
-  function getLocalDateString(date) {
-    return date.toLocaleDateString("nl-BE", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        weekday: 'long'
-    });
-}
-
-  const popupClose = (args) => {
-    if (args.type === "Editor" && !isNullOrUndefined(args.data)) {
-      let filmElement = args.element.querySelector("#Film");
-      if (filmElement) {
-        args.data.Film = filmElement.value;
-
-        var end = new Date(args.data.StartTime);
-        end.setHours(end.getHours() + 3);
-        args.data.EndTime = end;
-      }
-      let zaalElement = args.element.querySelector("#Zaal");
-      if (zaalElement) {
-        args.data.Zaal = zaalElement.value;
-      }
-      
-      //Geeft de datum terug voor in vertoningen te steken
-      //console.log(getLocalDateString(new Date(args.data.StartTime)).split(" ").slice(1).join(" "));
-      //Geeft de dag terug voor in vertoningen te steken
-      //console.log(getLocalDateString(new Date(args.data.StartTime)).split(" ")[0]);
-      //Geeft het uur terug voor in vertoningen te steken
-      //console.log(new Date(args.data.StartTime).toTimeString().substring(0,5))
-
-      console.log(movies.filter(movie => movie.isReleased).find(movie => movie.titel === args.data.Film));
-    }  
   };
 
   return (
